c073841acaacdc9d1ca68c4e9466f6b56a4dc1f0
aly8i <ABY00003@students.aust.edu.lb>
2022-11-15 20:50:26 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Card from '../components/card'
+import Card from '../components/Card'
 import Search from '../components/Search'
 import { useState,useEffect } from 'react'
 import axios from 'axios'
